ae491bdebf2aec59e97720232231dd99502cb27a
Nicklaus Ling <115532584+mnsling@users.noreply.github.com>
2024-02-21 21:34:38 +0800

    Update artistsphere.js

--- artistsphere.js
+++ artistsphere.js
@@ -1,32 +1,23 @@
-// script.js
-
 var currentlyDisplayedAlbums = null;
 var currentlyDisplayedSongs = null;
 
 function toggleAlbums(button, tableId) {
     var table = document.getElementById(tableId);
 
-    // Hide the previously displayed albums
     if (currentlyDisplayedAlbums) {
         currentlyDisplayedAlbums.style.display = "none";
     }
 
-    // Hide the previously displayed songs
     if (currentlyDisplayedSongs) {
         currentlyDisplayedSongs.style.display = "none";
     }
 
-    // Check if the table is currently hidden
     if (table.style.display === "none") {
-        // Show the table
         table.style.display = "block";
-        currentlyDisplayedAlbums = table; // Update the currently displayed albums
-        // Change button text to "Close Album"
+        currentlyDisplayedAlbums = table; 
     } else {
-        // Hide the table
         table.style.display = "none";
-        currentlyDisplayedAlbums = null; // Reset the currently displayed albums
-        // Change button text to "View Albums"
+        currentlyDisplayedAlbums = null; 
     }
 }
 
@@ -39,10 +30,10 @@ function showSongs(detailsId) {
     var details = document.getElementById(detailsId);
     if (details.style.display === "none") {
         details.style.display = "block";
-        currentlyDisplayedSongs = details; // Update the currently displayed songs
+        currentlyDisplayedSongs = details; 
     } else {
         details.style.display = "none";
-        currentlyDisplayedSongs = null; // Reset the currently displayed songs
+        currentlyDisplayedSongs = null; 
     }
 }
 
@@ -52,19 +43,16 @@ function showArtists(artistId) {
         artist.style.display = "none";
     });
 
-    // Hide the previously displayed albums when switching artists
     if (currentlyDisplayedAlbums) {
         currentlyDisplayedAlbums.style.display = "none";
     }
 
-    // Hide the previously displayed songs when switching artists
     if (currentlyDisplayedSongs) {
         currentlyDisplayedSongs.style.display = "none";
     }
 
-    // Show the selected table
     var artist = document.getElementById(artistId);
     artist.style.display = "block";
-    currentlyDisplayedAlbums = null; // Reset the currently displayed albums when switching artists
-    currentlyDisplayedSongs = null; // Reset the currently displayed songs when switching artists
+    currentlyDisplayedAlbums = null; 
+    currentlyDisplayedSongs = null; 
 }
